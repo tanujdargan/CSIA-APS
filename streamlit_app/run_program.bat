9b29e175fe4cb96d2dd09810ab17a165f9761e88
Tanuj Dargan <dargantanuj@gmail.com>
2023-02-06 00:00:55 +0530

    added new streamlit app

---
+++ streamlit_app/run_program.bat
@@ -0,0 +1,22 @@
+@echo OFF
+rem How to run a Python script in a given conda environment from a batch file.
+
+rem It doesn't require:
+rem - conda to be in the PATH
+rem - cmd.exe to be initialized with conda init
+
+rem Define here the path to your conda installation
+set CONDAPATH=C:\ProgramData\Anaconda3
+rem Define here the name of the environment
+set ENVNAME=CSIA_APS
+
+rem The following command activates the base environment.
+rem call C:\ProgramData\Miniconda3\Scripts\activate.bat C:\ProgramData\Miniconda3
+if %ENVNAME%==base (set ENVPATH=%CONDAPATH%) else (set ENVPATH=%CONDAPATH%\envs\%ENVNAME%)
+
+rem Activate the conda environment
+rem Using call is required here, see: https://stackoverflow.com/questions/24678144/conda-environments-and-bat-files
+call %CONDAPATH%\Scripts\activate.bat %ENVPATH%
+
+rem Run a python script in that environment
+streamlit run streamlit_app.py
